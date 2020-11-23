bf750de720dd66cdfc145ad66ff14f0457f74055
tmiya061 <63568444+tmiya061@users.noreply.github.com>
2020-11-24 04:28:08 +0900

    modal

--- scripts/vendors/modal.js
+++ scripts/vendors/modal.js
@@ -1,21 +1,14 @@
-function popupImage() {
-  var popup = document.getElementById('js-popup');
-  if(!popup) return;
+$(function () {
+  // モーダルのボタンをクリックした時
+  $('.modal_btn').on('click', function() {
+    var btnIndex = $(this).index(); // 何番目のモーダルボタンかを取得
+    $('.modal_area .modal_box').eq(btnIndex).fadeIn(); // クリックしたモーダルボタンと同じ番目のモーダルを表示する
+    $('body').addClass('is-fixed')
+  });
 
-  var blackBg = document.getElementById('js-black-bg');
-
-  var blackBg = document.getElementById('js-black-bg');
-  var closeBtn = document.getElementById('js-close-btn');
-  var showBtn = document.getElementById('js-show-popup');
-
-  closePopUp(blackBg);
-  closePopUp(closeBtn);
-  closePopUp(showBtn);
-  function closePopUp(elem) {
-    if(!elem) return;
-    elem.addEventListener('click', function() {
-      popup.classList.toggle('is-show');
-    });
-  }
-}
-popupImage();
+  // ×やモーダルの背景をクリックした時
+  $('.modal_close , .modal_bg').click(function(){
+    $('.modal_box').fadeOut(); // モーダルを非表示にする
+    $('.body').removeClass('is-fixed')
+  });
+});
--- scripts/vendors/scroll-link.js
+++ scripts/vendors/scroll-link.js
@@ -1,17 +1,27 @@
+// $(function(){
+//   // #で始まるアンカーをクリックした場合に処理
+//   $('a[href^=#]').click(function() {
+//      // スクロールの速度
+//      let speed = 400; // ミリ秒
+//      // アンカーの値取得
+//      let href= $(this).attr("href");
+//      // 移動先を取得
+//      let target = $(href == "#" || href == "" ? 'html' : href);
+//      // 移動先を数値で取得
+//      let position = target.offset().top - 50;
+//      // スムーススクロール
+//      $('body,html').animate({scrollTop:position}, speed, 'swing');
+//      return false;
+//   });
+// });
+
 $(function(){
-  // #で始まるアンカーをクリックした場合に処理
-  $('a[href^=#]').click(function() {
-     // スクロールの速度
-     var speed = 400; // ミリ秒
-     // アンカーの値取得
-     var href= $(this).attr("href");
-     // 移動先を取得
-     var target = $(href == "#" || href == "" ? 'html' : href);
-     // 移動先を数値で取得
-     var position = target.offset().top - 50;
-     // スムーススクロール
-     $('body,html').animate({scrollTop:position}, speed, 'swing');
-     return false;
+  $('a[href^="#"]').click(function(){
+      var speed = 500;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      $("html, body").animate({scrollTop:position}, speed, "swing");
+      return false;
   });
 });
-
